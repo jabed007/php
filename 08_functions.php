347f156b0d32d100071a2d783c0f848939478c89
Md. Jabed Hossan <jabed.cse.21@gmail.com>
2022-12-17 12:20:03 +0600

    php function example

--- 08_functions.php
+++ 08_functions.php
@@ -7,5 +7,16 @@
 // Create sum of two functions
 
 // Create function to sum all numbers using ...$nums
+function sum (...$nums) {
+  // echo "<pre>";
+  // print_r($nums);
+  // echo "</pre>";
+  $sum = 0;
+  foreach($nums as $num) {
+    $sum += $num;
+  }
+  return $sum;
+}
 
+echo sum(1, 2, 3, 4, 5);
 // Arrow functions
